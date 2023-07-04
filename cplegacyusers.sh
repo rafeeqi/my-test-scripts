d120adbac5b16c68449862f264cd12ddb027feea
ibrahimrafeeq <ibrahim.rafeeq@gmail.com>
2023-07-04 19:29:33 +0530

    my ansible 3

--- cplegacyusers.sh
+++ cplegacyusers.sh
@@ -5,6 +5,7 @@
 rtmptdir="/home/$USER/tmptdir"
 ltmptdir="/home/$USER/authkey_tmpdir"
 
+#Replace host with original hostname
 host1="host"
 host2="host"
 
@@ -12,8 +13,7 @@ host2="host"
 mkdir -p "$ltmptdir"
 rm -f $ltmptdir/*
 
-#Create remote temp directories
-
+#Create remote temp directories on remote hosts
 ssh $host1 "mkdir -p $rtmptdir && rm -rf $rtmptdir/*"
 ssh $host2 "mkdir -p $rtmptdir && rm -rf $rtmptdir/*"
 
@@ -23,20 +23,19 @@ usernames_host1=$(ssh $host1 ls -l /home | awk '{print $3}' | tail -n +2 | grep 
 # Fetch usernames from host2
 usernames_host2=$(ssh $host2 ls -l /home | awk '{print $3}' | tail -n +2| grep -Ev 'root|ubuntu|azureuser|lg_admin|www-upload|$USER|webscalebuilder')
 
-# Create usernames on host2
+# Create usernames on host2 that exist on host1 but not on host2
 for username in $usernames_host1; do
     if [[ ! $usernames_host2 =~ $username ]]; then
-        echo "Adding user $username on $host2"
-        sleep 1
-        ssh $host2 "sudo useradd -m -s /bin/bash -g www-data $username && sudo mkdir -p /home/$username/.ssh/ && sudo chmod 0700 /home/$username/.ssh/"
-        echo "Taking copy of auth key file"
-        ssh $host1 "sudo cp /home/$username/.ssh/authorized_keys $rtmptdir/ && sudo chown $USER:$USER $rtmptdir/authorized_keys"
-        echo "pulling auth key to local"
-        scp $host1:$rtmptdir/authorized_keys $ltmptdir/
-        echo "pushing auth key to remote"
-        scp $ltmptdir/authorized_keys $host2:$rtmptdir/
-        echo "replacing authkey on host2"
-        ssh $host2 "sudo mv $rtmptdir/authorized_keys /home/$username/.ssh/authorized_keys && sudo chmod 0600 /home/$username/.ssh/authorized_keys && sudo chown -R $username:www-data /home/$username/.ssh"
+    echo "Adding user $username on $host2"
+    ssh $host2 "sudo useradd -m -s /bin/bash -g www-data $username && sudo mkdir -p /home/$username/.ssh/ && sudo chmod 0700 /home/$username/.ssh/"
+    echo "Taking copy of auth key file on host1"
+    ssh $host1 "sudo cp /home/$username/.ssh/authorized_keys $rtmptdir/ && sudo chown $USER:$USER $rtmptdir/authorized_keys"
+    echo "pulling auth key to local"
+    scp $host1:$rtmptdir/authorized_keys $ltmptdir/
+    echo "pushing auth key to remote host2"
+    scp $ltmptdir/authorized_keys $host2:$rtmptdir/
+    echo "replacing authkey on host2"
+    ssh $host2 "sudo mv $rtmptdir/authorized_keys /home/$username/.ssh/authorized_keys && sudo chmod 0600 /home/$username/.ssh/authorized_keys && sudo chown -R $username:www-data /home/$username/.ssh"
     fi
 done
 
